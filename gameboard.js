b362b0a094451c6046bfd406a08660749300e800
jjuneau13 <161267313+jjuneau13@users.noreply.github.com>
2026-08-15 10:42:25 -0400

    Finished receiveAttack in Gameboard class

--- gameboard.js
+++ gameboard.js
@@ -18,16 +18,6 @@ export class Gameboard {
 
     placeShip(coord, ship, vert = false) {
         if (vert == false) {
-            if (coord[1] + this.ships[ship].length > 10)
-                throw new Error("Placed boat outside grid");
-            for (
-                let i = coord[1];
-                i < coord[1] + this.ships[ship].length;
-                i++
-            ) {
-                this.board[coord[0]][i] = ship;
-            }
-        } else {
             if (coord[0] + this.ships[ship].length > 10)
                 throw new Error("Placed boat outside grid");
             for (
@@ -35,8 +25,35 @@ export class Gameboard {
                 i < coord[0] + this.ships[ship].length;
                 i++
             ) {
-                this.board[i][coord[1]] = ship;
+                this.board[coord[1]][i] = ship;
             }
+        } else {
+            if (coord[1] + this.ships[ship].length > 10)
+                throw new Error("Placed boat outside grid");
+            for (
+                let i = coord[1];
+                i < coord[1] + this.ships[ship].length;
+                i++
+            ) {
+                this.board[i][coord[0]] = ship;
+            }
+        }
+    }
+
+    receiveAttack(x, y) {
+        if (x < 0 || x > 9 || y < 0 || y > 9) {
+            throw new Error("Cannot attack outside grid");
+        }
+        if (this.board[y][x] == "X" || this.board[y][x] == "M") {
+            throw new Error("Already attacked this tile");
+        }
+        if (this.board[y][x] == 0) {
+            this.board[y][x] = "M";
+            return "Miss";
+        } else {
+            this.ships[this.board[y][x]].hit();
+            this.board[y][x] = "X";
+            return "Hit!";
         }
     }
 
--- ship.spec.js
+++ ship.spec.js
@@ -18,8 +18,9 @@ describe("Gameboard testing", () => {
     beforeEach(() => {
         testGameboard = new Gameboard();
     });
+
     test("Placing horizontal ship in gameboard", () => {
-        testGameboard.placeShip([1, 4], "B");
+        testGameboard.placeShip([4, 1], "B");
         expect(testGameboard.getBoard()).toEqual([
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, "B", "B", "B", "B", 0, 0],
@@ -33,8 +34,9 @@ describe("Gameboard testing", () => {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]);
     });
+
     test("Placing vertical ship on gameboard", () => {
-        testGameboard.placeShip([3, 2], "D", true);
+        testGameboard.placeShip([2, 3], "D", true);
         expect(testGameboard.getBoard()).toEqual([
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -48,8 +50,9 @@ describe("Gameboard testing", () => {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]);
     });
+
     test("Placing ship near edge", () => {
-        testGameboard.placeShip([9, 5], "A");
+        testGameboard.placeShip([5, 9], "A");
         expect(testGameboard.getBoard()).toEqual([
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -63,10 +66,31 @@ describe("Gameboard testing", () => {
             [0, 0, 0, 0, 0, "A", "A", "A", "A", "A"],
         ]);
     });
+
     test("Placing horizontal ship outside border throws error", () => {
         expect(() => testGameboard.placeShip([9, 6], "A")).toThrow(Error);
     });
+
     test("Placing vertical ship outside border throws error", () => {
         expect(() => testGameboard.placeShip([6, 9], "A")).toThrow(Error);
+    });
+
+    test("Attacks correct tile", () => {
+        testGameboard.placeShip([3, 4], "A");
+        expect(testGameboard.receiveAttack(3, 4)).toBe("Hit!");
+    });
+
+    test("Correctly attacks empty tile", () => {
+        testGameboard.placeShip([3, 5], "D");
+        expect(testGameboard.receiveAttack(3, 6)).toBe("Miss");
+    });
+
+    test("Throws error when attacking outside of grid", () => {
+        expect(() => testGameboard.receiveAttack(10, 0)).toThrow(Error);
+    });
+
+    test("Throws error when attacking the same spot twice", () => {
+        testGameboard.receiveAttack(4, 6);
+        expect(() => testGameboard.receiveAttack(4, 6)).toThrow(Error);
     });
 });
